d81c58c40f44fb63ad7fe9a0a846d30d669c3676
Islam-13 <islam-hussein@outlook.com>
2025-05-19 15:10:42 +0300

    add tailwind remove primeflex

---
+++ apps/elevate-e-commerce-app/src/app/pages/components/best-seller/best-seller.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-best-seller',
+  imports: [CommonModule],
+  templateUrl: './best-seller.component.html',
+  styleUrl: './best-seller.component.css',
+})
+export class BestSellerComponent {}
---
+++ apps/elevate-e-commerce-app/src/app/pages/components/trusted-by/trusted-by.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-trusted-by',
+  imports: [CommonModule],
+  templateUrl: './trusted-by.component.html',
+  styleUrl: './trusted-by.component.css',
+})
+export class TrustedByComponent {}
