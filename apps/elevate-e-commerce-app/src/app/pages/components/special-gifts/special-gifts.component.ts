2c57b4104cc5d81d9ec50cb426bedd11277053c4
Ahmed-Osama-Elfeky <Elfekya166@gmail.com>
2025-05-21 09:33:47 +0300

    Special-Gifts Phase 1 (need to edit in Carousel Owl )

---
+++ apps/elevate-e-commerce-app/src/app/pages/components/special-gifts/special-gifts.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-special-gifts',
+  imports: [CommonModule,CarouselModule],
+  templateUrl: './special-gifts.component.html',
+  styleUrl: './special-gifts.component.css',
+})
+export class SpecialGiftsComponent {
+
+mainSliderOptions: OwlOptions = {
+   loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 700,
+    navText: ['<', '>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      
+    },
+    nav: true
+  }
+   // Image array
+  imageSlides = [
+    { id: 1, src: './images/special-gifts/Main-Background.jpg', alt: 'Main' },
+    { id: 2, src: './images/special-gifts/Background1.jpg', alt: 'Image 1' },
+    { id: 3, src: './images/special-gifts/Background2.jpg.jpg', alt: 'Image 2' },
+    { id: 4, src: './images/special-gifts/Background3.jpg', alt: 'Image 3' },
+    { id: 5, src: './images/special-gifts/Background4.jpg', alt: 'Image 4' },
+    { id: 6, src: './images/special-gifts/Background5.jpg', alt: 'Image 5' },
+    { id: 7, src: './images/special-gifts/Background6.jpg', alt: 'Image 6' },
+
+  ];
+
+
+
+
+   onButtonClick() {
+    alert('Button clicked!');
+  }
+
+}
--- apps/elevate-e-commerce-app/src/app/pages/home/home.component.ts
+++ apps/elevate-e-commerce-app/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SpecialGiftsComponent } from "../components/special-gifts/special-gifts.component";
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule],
+  imports: [CommonModule, SpecialGiftsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
