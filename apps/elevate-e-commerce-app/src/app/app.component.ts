e4ce3fb115b595b99089a7c09306fb432526f806
ayayahyaa <ayayahya232@gmail.com>
2025-05-24 01:26:50 +0300

    finsh categories

--- apps/elevate-e-commerce-app/src/app/app.component.ts
+++ apps/elevate-e-commerce-app/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+<<<<<<< HEAD
 import { HomeComponent } from "./pages/home/home.component";
 import { CategoriesComponent } from "./pages/components/categories/categories.component";
 import { PopularItemsComponent } from "./pages/components/popular-items/popular-items.component";
+=======
+import { HomeComponent } from './pages/home/home.component';
+>>>>>>> ea97fdb691afe9b86c0eb2622fad8efb1f5daca1
 
 @Component({
   imports: [HomeComponent, CategoriesComponent, PopularItemsComponent],
--- apps/elevate-e-commerce-app/src/app/pages/components/trusted-by/trusted-by.component.ts
+++ apps/elevate-e-commerce-app/src/app/pages/components/trusted-by/trusted-by.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-trusted-by',
-  imports: [CommonModule],
   templateUrl: './trusted-by.component.html',
   styleUrl: './trusted-by.component.css',
 })
-export class TrustedByComponent {}
+export class TrustedByComponent {
+  images = [
+    { src: '/images/trustedBy/image 41.png' },
+    { src: '/images/trustedBy/image 40.png' },
+    { src: '/images/trustedBy/image 39.png' },
+    { src: '/images/trustedBy/image 38.png' },
+    { src: '/images/trustedBy/image 37.png' },
+    { src: '/images/trustedBy/image 36.png' },
+  ];
+
+  settings = {
+    '--numItems': this.images.length,
+    '--width': '150px',
+    '--height': '50px',
+    '--speed': '20s',
+    '--gap': '20px',
+    '--single-slide-speed': 'calc(var(--speed) / var(--numItems))',
+    '--track-width':
+      'calc((var(--width) + var(--gap)) * (var(--numItems) - 1))',
+  };
+}
