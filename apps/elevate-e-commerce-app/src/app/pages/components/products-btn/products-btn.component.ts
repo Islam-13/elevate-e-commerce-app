b3dbf3d1c81d79a3f0e5ff7e9c7f780a5b6c5f3e
ayayahyaa <ayayahya232@gmail.com>
2025-05-30 20:27:21 +0300

    new commit

---
+++ apps/elevate-e-commerce-app/src/app/pages/components/products-btn/products-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-products-btn',
+  imports: [CommonModule],
+  templateUrl: './products-btn.component.html',
+  styleUrl: './products-btn.component.css',
+})
+export class ProductsBtnComponent {}
