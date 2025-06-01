9cd326a858a12ed87bdbbd1ef4f853fb92a6fbef
Ahmed-Aly22 <ahmedaliabdelqader2@gmail.com>
2025-06-01 03:12:45 +0300

    navbar

---
+++ apps/elevate-e-commerce-app/src/app/pages/About/About.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about',
+  imports: [CommonModule],
+  templateUrl: './About.component.html',
+  styleUrl: './About.component.css',
+})
+export class AboutComponent {}
---
+++ apps/elevate-e-commerce-app/src/app/pages/AllCategory/AllCategory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-all-category',
+  imports: [CommonModule],
+  templateUrl: './AllCategory.component.html',
+  styleUrl: './AllCategory.component.css',
+})
+export class AllCategoryComponent {}
