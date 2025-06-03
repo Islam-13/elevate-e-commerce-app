89659ab525fb8ed101c894b4e0f88945b51cf1cd
Ahmed-Aly22 <ahmedaliabdelqader2@gmail.com>
2025-06-03 07:01:19 +0300

    after fix

--- apps/elevate-e-commerce-app/src/app/app.component.ts
+++ apps/elevate-e-commerce-app/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/ui/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '@shared/ui/navbar/navbar.component';
 
 @Component({
-  imports: [HomeComponent, FooterComponent],
+  imports: [HomeComponent, FooterComponent,RouterOutlet,NavbarComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
