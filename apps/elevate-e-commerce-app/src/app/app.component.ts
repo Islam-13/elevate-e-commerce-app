610140597f21096aeae8fe2e3a586633aab89be5
Islam-13 <islam-hussein@outlook.com>
2025-06-06 09:17:12 +0300

    add dark mode feature

--- apps/elevate-e-commerce-app/src/app/app.component.ts
+++ apps/elevate-e-commerce-app/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from '@shared/ui/footer/footer.component';
-import { LocalStorageService } from '@shared/services/localStorage/local-storage.service';
+import { ThemeService } from '@shared/services/theme/theme.service';
 
 @Component({
   imports: [HomeComponent, FooterComponent],
@@ -10,49 +10,12 @@ import { LocalStorageService } from '@shared/services/localStorage/local-storage
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'elevate-e-commerce-app';
-  isDark = signal<boolean>(false);
 
-  private _localStorage = inject(LocalStorageService);
+  _theme = inject(ThemeService);
 
-  ngOnInit(): void {
-    this.themeMode();
-  }
-
-  themeMode() {
-    if (this._localStorage.isBrowser) {
-      const theme = this._localStorage.get('theme');
-
-      if (theme) {
-        if (theme === 'dark') {
-          this.dark();
-        } else this.light();
-      } else {
-        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-          this.dark();
-        } else {
-          this.light();
-        }
-      }
-    }
-  }
-
-  light() {
-    document.documentElement.classList.add('light');
-    document.documentElement.classList.remove('dark');
-
-    this.isDark.set(false);
-
-    this._localStorage.set('theme', 'light');
-  }
-
-  dark() {
-    document.documentElement.classList.add('dark');
-    document.documentElement.classList.remove('light');
-
-    this.isDark.set(true);
-
-    this._localStorage.set('theme', 'dark');
+  toggleTheme() {
+    this._theme.toggleTheme();
   }
 }
