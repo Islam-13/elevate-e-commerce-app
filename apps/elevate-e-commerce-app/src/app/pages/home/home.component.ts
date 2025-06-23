a8eebf09a90163d2a5c0ec5e44d9d2188b2b5c58
Islam-13 <islam-hussein@outlook.com>
2025-06-23 15:05:22 +0300

    add category page - aside ui

--- apps/elevate-e-commerce-app/src/app/pages/home/home.component.ts
+++ apps/elevate-e-commerce-app/src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesComponent } from '../components/categories/categories.component';
@@ -8,6 +8,8 @@ import { TrustedByComponent } from '../components/trusted-by/trusted-by.componen
 import { FeaturesComponent } from '../components/features/features.component';
 import { PopularItemsComponent } from '../components/popular-items/popular-items.component';
 import { AboutUsComponent } from '../../shared/ui/about-us/about-us.component';
+import { Category } from '@shared/interfaces/categories-interfaces/categories-interfaces';
+import { CategoriesService } from '@shared/services/categories/categories.service';
 
 @Component({
   selector: 'app-home',
@@ -17,11 +19,37 @@ import { AboutUsComponent } from '../../shared/ui/about-us/about-us.component';
     SpecialGiftsComponent,
     TrustedByComponent,
     BestSellerComponent,
-    FeaturesComponent,
     PopularItemsComponent,
+    FeaturesComponent,
     AboutUsComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  categories = signal<Category[]>([]);
+
+  private readonly _categoriesService = inject(CategoriesService);
+  private readonly _destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    const subscription = this._categoriesService.getAllCategories().subscribe({
+      next: (data) => {
+        console.log(data);
+        this.categories.set(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('categories complated');
+      },
+    });
+
+    this._destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
+}
