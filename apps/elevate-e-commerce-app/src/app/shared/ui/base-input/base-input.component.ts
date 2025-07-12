999050e4f085f14495f2890a6db8db07ed72e857
saad <saad>
2025-07-12 17:15:29 +0300

    use ControlValueAccessor

---
+++ apps/elevate-e-commerce-app/src/app/shared/ui/base-input/base-input.component.ts
@@ -0,0 +1,49 @@
+import { Component, forwardRef, input, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-base-input',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './base-input.component.html',
+  styleUrl: './base-input.component.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => BaseInputComponent),
+      multi: true
+    }
+  ]
+})
+export class BaseInputComponent implements ControlValueAccessor {
+
+  /* ===== required API props ===== */
+  type        = input<'text' | 'email' | 'password'>('text');
+  placeholder = input<string>(' ');
+
+  /* ===== internal state (signals) ===== */
+  value    = signal<string>('');
+  disabled = signal<boolean>(false);
+
+  writeValue(v: string | null): void {
+    this.value.set(v ?? '');
+  }
+  registerOnChange(fn: (v: string) => void): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled.set(isDisabled);
+  }
+
+  onChange: (v: string) => void = () => {};
+  onTouched: () => void = () => {};
+
+  onInput(e: Event) {
+    const text = (e.target as HTMLInputElement).value;
+    this.onChange(text);
+  }
+}
