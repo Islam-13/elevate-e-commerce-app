fbdb148997f90dbd8297c6cf804735afbf8b76ce
ayayahyaa <ayayahya232@gmail.com>
2025-07-13 22:08:19 +0300

    update in filtration

--- apps/elevate-e-commerce-app/src/app/pages/allproducts/allproducts.component.ts
+++ apps/elevate-e-commerce-app/src/app/pages/allproducts/allproducts.component.ts
@@ -1,72 +1,56 @@
-import { Component, inject, computed } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { CardComponent } from '../../shared/ui/card/card.component';
-import { Store } from '@ngrx/store';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { Product } from '@shared/interfaces/popular-items-interface/popular-items-interface';
+import { PopularItemsService } from '@shared/services/popular-items/popular-items.service';
+import { Subscription } from 'rxjs';
+import { CardComponent } from "../../shared/ui/card/card.component";
 import { FormsModule } from '@angular/forms';
-import { selectFilterProducts, selectSelectedCategories, selectSelectedOccasions, selectSelectedPrice, selectSelectedRating } from '../../store/filter.selector';
+
+
+
+
 
 @Component({
   selector: 'app-allproducts',
-  standalone: true,
-  imports: [CommonModule, CardComponent, FormsModule],
+  imports: [CardComponent,FormsModule],
   templateUrl: './allproducts.component.html',
-  styleUrl: './allproducts.component.css',
+  styleUrl: './allproducts.component.css'
 })
-export class AllproductsComponent  {
-  private readonly store = inject(Store);
+export class AllproductsComponent implements OnInit, OnDestroy {
 
-  selectedSortOption = 'priceAsc';
+  private readonly _popularItemsService = inject(PopularItemsService);
 
-  categories = this.store.selectSignal(selectSelectedCategories);
-  occasions = this.store.selectSignal(selectSelectedOccasions);
-  rating = this.store.selectSignal(selectSelectedRating);
-  price = this.store.selectSignal(selectSelectedPrice);
+  allProducts = signal<Product[]>([]);
+  private subscription!: Subscription;
+  selectedSortOption: string = 'priceAsc';
 
-  filteredProducts = this.store.selectSignal(selectFilterProducts);
 
-  allProducts = computed(() => {
-    let result = [...this.filteredProducts()];
-
-    const selectedCategories = this.categories();
-    const selectedOccasions = this.occasions();
-    const selectedRating = this.rating();
-    const selectedPrice = this.price();
-
-    if (selectedCategories.length > 0) {
-      const catIds = selectedCategories.map((c) => c._id);
-      result = result.filter((p) => catIds.includes(p.category));
-    }
-
-    if (selectedOccasions.length > 0) {
-      const occIds = selectedOccasions.map((o) => o._id);
-      result = result.filter((p) => occIds.includes(p.occasion));
-    }
-
-    if (selectedPrice) {
-      result = result.filter(
-        (p) =>
-          p.priceAfterDiscount >= selectedPrice.minPrice &&
-          p.priceAfterDiscount <= selectedPrice.maxPrice
-      );
-    }
-
-    if (selectedRating.length > 0) {
-      const ratingValues = selectedRating.map((r) => r.rating || 0);
-      result = result.filter((p) => ratingValues.includes(Math.round(p.rateAvg)));
-    }
-
-    if (this.selectedSortOption === 'priceAsc') {
-      result.sort((a, b) => a.priceAfterDiscount - b.priceAfterDiscount);
-    } else if (this.selectedSortOption === 'priceDesc') {
-      result.sort((a, b) => b.priceAfterDiscount - a.priceAfterDiscount);
-    }
-
-    return result;
-  });
-
-  sortProducts(option: string): void {
-  this.selectedSortOption = option;
+  ngOnInit(): void {
+    this.getAllProducts();
 }
 
+  getAllProducts(): void {
+    this.subscription = this._popularItemsService.getAllProducts({
+      sort: 'priceAfterDiscount'
+    }).subscribe((data) => {
 
+      this.allProducts.set(data.products);
+      console.log('All Products:', this.allProducts());
+
+  });
+  }
+
+  sortProducts(option: string): void {
+if (option === 'priceAsc') {
+      this.allProducts.set([...this.allProducts()].sort((a, b) => a.priceAfterDiscount - b.priceAfterDiscount));
+    } else if (option === 'priceDesc') {
+      this.allProducts.set([...this.allProducts()].sort((a, b) => b.priceAfterDiscount - a.priceAfterDiscount));
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+
+      this.subscription.unsubscribe();
+    }
+  }
 }
