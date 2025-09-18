2be93f25d0a5df5346bee48aeb3dff39ef31968b
FatmaElhosary <engfatmafathy@gmail.com>
2025-09-18 11:29:47 +0300

    created New dashboard app added routing

---
+++ ecommerceDashboard/src/app/core/navigation/components/navigation/navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+
+@Component({
+  selector: 'app-navigation',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.css',
+})
+export class NavigationComponent {}
