71cf2ee1402849f08fe1ea09d002378cb9244204
FatmaElhosary <engfatmafathy@gmail.com>
2025-09-19 19:53:08 +0300

    enhanced design

--- ecommerceDashboard/src/app/core/navigation/components/navigation/navigation.component.ts
+++ ecommerceDashboard/src/app/core/navigation/components/navigation/navigation.component.ts
@@ -1,42 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MenuModule } from 'primeng/menu';
-import { Menu } from 'primeng/menu';
-import { MenuItem } from 'primeng/api';
 import { UiButtonComponent } from './../../../../../../../ui-button/src/lib/ui-button/ui-button.component';
+import { UserImageComponent } from '../business/user-image/user-image.component';
 
 @Component({
   selector: 'app-navigation',
-  imports: [CommonModule, RouterModule, MenuModule, Menu, UiButtonComponent],
+  imports: [CommonModule, RouterModule, UiButtonComponent, UserImageComponent],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
 export class NavigationComponent implements OnInit {
-  items: MenuItem[] | undefined;
-  ngOnInit(): void {
-    this.items = [
-      {
-        items: [
-          {
-            label: 'Profile',
-            icon: 'pi pi-refresh',
-            routerLink: '/profile',
-            styleClass: 'text-black  ',
-          },
-          {
-            label: 'Logout',
-            icon: 'pi pi-upload',
-            styleClass: 'text-red  ',
-
-            command: () => {
-              this.logout();
-            },
-          },
-        ],
-      },
-    ];
-  }
-
-  logout() {}
+  ngOnInit(): void {}
 }
