a3b64729bb4a4c038a277068e2d1b04fcc45fa02
FatmaElhosary <engfatmafathy@gmail.com>
2025-09-19 22:44:37 +0300

    created dashboard app and finished navigation design

---
+++ apps/dashboard/src/app/app.routes.ts
@@ -0,0 +1,22 @@
+import { Route } from '@angular/router';
+
+export const appRoutes: Route[] = [
+     {
+    path: '',
+    loadComponent: () =>
+      import(
+        './core/navigation/components/navigation/navigation.component'
+      ).then((c) => c.NavigationComponent),
+    children: [
+      { path: '', redirectTo: 'Dashboard', pathMatch: 'full' }, // default route
+
+      {
+        path: 'Dashboard',
+        loadComponent: () =>
+          import(
+            './features/overView/components/overview/overview.component'
+          ).then((c) => c.OverviewComponent),
+      },
+    ],
+  },
+];
---
+++ libs/unauthorized-page/src/lib/unauthorized-page/unauthorized-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-unauthorized-page',
+  imports: [CommonModule],
+  templateUrl: './unauthorized-page.component.html',
+  styleUrl: './unauthorized-page.component.css',
+})
+export class UnauthorizedPageComponent {}
