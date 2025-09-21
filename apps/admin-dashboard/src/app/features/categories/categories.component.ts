d6ffc45d89a33877343a44377a84aba6fcf3195a
Islam-13 <islam-hussein@outlook.com>
2025-09-22 00:54:31 +0300

    fix table err

--- apps/admin-dashboard/src/app/features/categories/categories.component.ts
+++ apps/admin-dashboard/src/app/features/categories/categories.component.ts
@@ -5,17 +5,24 @@ import { FeatureHeadingComponent } from '../components/feature-heading/feature-h
 import { Category } from '../../shared/types/categories';
 import { CategoriesService } from '../../shared/services/categories/categories.service';
 import { ActionBtnsComponent } from '../components/action-btns/action-btns.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-categories',
-  imports: [FeatureHeadingComponent, ActionBtnsComponent, TableModule],
+  imports: [
+    FeatureHeadingComponent,
+    ActionBtnsComponent,
+    TableModule,
+    FormsModule,
+  ],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css',
 })
 export class CategoriesComponent implements OnInit {
   categories = signal<Category[]>([]);
   openId = signal<string>('');
-  loading = signal<boolean>(false);
+  searchValue = signal<string>('');
+  isLoading = signal<boolean>(false);
 
   private readonly _categoriesService = inject(CategoriesService);
   private readonly _destroyRef = inject(DestroyRef);
@@ -25,6 +32,7 @@ export class CategoriesComponent implements OnInit {
   }
 
   getAllCategories() {
+    this.isLoading.set(true);
     const subscription = this._categoriesService.getAllCategories().subscribe({
       next: (data) => {
         this.categories.set(data);
@@ -32,6 +40,7 @@ export class CategoriesComponent implements OnInit {
       error: () => {
         console.log('test');
       },
+      complete: () => this.isLoading.set(false),
     });
 
     this._destroyRef.onDestroy(() => subscription.unsubscribe());
--- apps/admin-dashboard/src/app/features/components/feature-search/feature-search.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-feature-search',
-  imports: [],
-  templateUrl: './feature-search.component.html',
-  styleUrl: './feature-search.component.css',
-})
-export class FeatureSearchComponent {}
